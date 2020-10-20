ff5fa7017860e6740eca4173e6eccd087108533f
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-21 01:09:45 +0300

    add script

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-// import './styles.css';
 import './js/color-switch.js';
--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -1,18 +1,12 @@
+import { colors } from './colors.js';
+
 const refs = {
   body: document.querySelector('body'),
   btnStart: document.querySelector('[data-action="start"]'),
   btnStop: document.querySelector('[data-action="stop"]'),
-}
+};
 
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
-
+let intervalId = null;
 
 refs.btnStart.addEventListener('click', onClickColorSwitchOn);
 
@@ -21,25 +15,18 @@ refs.btnStop.addEventListener('click', onClickColorSwitchOff);
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
 
-const timerId = setInterval(callback, delay, arg1, arg2, ...);
+function onClickColorSwitchOn() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+  }, 1000)
 
+  refs.btnStart.disabled = true;
 
-function onClickColorSwitchOn(evt) {
-  console.log(evt.currentTarget)
-  console.log(evt.target)
-
-  // setInterval()
-  refs.body.style.backgroundColor = randomColor;
 };
 
 function onClickColorSwitchOff() {
-console.log('onClickColorSwitchOff')
+  clearInterval(intervalId)
+
+  refs.btnStart.disabled = false;
 };
-
-
-console.log(randomColor)
-function colorSwitch() {
-  colors.forEach
-}
